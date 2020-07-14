d93de3f15873e2387df95fd9c2cc50e063100ad7
weikiat97 <jerryloh97@hotmail.com>
2020-07-15 01:18:11 +0800

    Level 4 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,33 +37,39 @@ app.get("/login", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err) {
+            if (!err) {
+                res.render("secrets");
+            } else {
+                res.send(err);
+            }
+        });
     });
-    newUser.save(function(err) {
-        if (!err) {
-            res.render("secrets");
-        } else {
-            res.send(err);
-        }
-    });
+    
 })
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
-    console.log(password);
+    const password = req.body.password;
+
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
             res.send(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                } else {
-                    res.send("Wrong password! Try again!");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        res.send("Wrong password! Try again!");
+                    }
+                });
             } else {
                 res.send("No such user! Please register instead");
             }
